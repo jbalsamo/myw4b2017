a8859d8eaf6b539106ea811204268fd3f325ac8b
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2017-08-22 15:51:45 -0400

    Added chooser on main page.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-var hello = function(name) {
+function hello(name) {
     namearea = document.getElementById("namearea");
-    namearea.innerHTML(name);
-};
+    namearea.innerHTML = name;
+}
 
-$(document).ready(function() {
-    this.hello("Joe");
+$(document).ready(function () {
+    hello("Joe");
+    cbio.getCancerStudies(function (x) {
+        x.map(function (a) {
+            id = a.split("\t")[0];
+            name = a.split("\t")[1];
+            if (name != "name") {
+                c = {
+                    'id': id,
+                    'name': name
+                };
+                $("#clist").append('<option value="' + c.id + '">' + c.name + '</option>');
+            }
+        });
+        $('select').material_select();
+    });
 });
